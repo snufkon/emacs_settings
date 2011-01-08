19c5581cafbb1692651b6e78b0e851ebde855fbe
snufkon <snufkon@gmail.com>
2011-01-08 15:45:11 +0900

    .rake拡張子をrubyモードで開くように設定

--- dot_emacs/mode/ruby-mode-settings.el
+++ dot_emacs/mode/ruby-mode-settings.el
@@ -2,6 +2,7 @@
 (autoload 'ruby-mode "ruby-mode" "Mode for editing ruby source files" t)
 ;(require 'ruby-mode)
 (setq auto-mode-alist (cons '("\\.rb$" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.rake$" . ruby-mode) auto-mode-alist))
 (setq interpreter-mode-alist (append '(("ruby" . ruby-mode)) interpreter-mode-alist))
 (autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
 (autoload 'inf-ruby-keys "inf-ruby" "Set local key defs for inf-ruby in ruby-mode")
