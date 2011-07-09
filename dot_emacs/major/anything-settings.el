c3e0196281e366194f9e98c81cc2590f6f5f9d16
近藤 聡 <kondo@kondo.local>
2011-07-09 17:59:41 +0900

    change

--- dot_emacs/major/anything-settings.el
+++ dot_emacs/major/anything-settings.el
@@ -1,28 +1,70 @@
 ;;; anything -------------------------------------------------------------------
-(require 'anything)
-(require 'anything-config)
-(require 'anything-match-plugin)
-(require 'anything-etags)
-(require 'anything-yaetags)
-(require 'anything-gtags)
-(require 'anything-ipa)
+(require 'anything-startup)
+;(require 'anything-etags)
+;(require 'anything-yaetags)
+;(require 'anything-gtags)
+;(require 'anything-ipa)
 
 ;; 自動でimenuのインデックスを作成
 (setq imenu-auto-rescan t)
 (global-set-key (kbd "C-x b") 'anything)
 
-;; anythingの検索対象を設定 
-(setq anything-sources
-      '(anything-c-source-buffers+
-	anything-c-source-file-name-history
-	anything-c-source-buffer-not-found
-	anything-c-source-imenu
-	anything-c-source-etags-select
-	anything-c-source-plocate
+
+;;; 複数ファイルにまたがりbm.elを検索対象に追加する設定 ------------------------
+;;; 参考: 
+;;; - http://d.hatena.ne.jp/peccu/20100402/bmglobal
+(defvar anything-c-source-bm-global-use-candidates-in-buffer
+  '((name . "Global Bookmarks")
+    (init . anything-c-bm-global-init)
+    (candidates-in-buffer)
+    (type . file-line))
+  "show global bookmarks list.
+Global means All bookmarks exist in `bm-repository'.
+
+Needs bm.el.
+http://www.nongnu.org/bm/")
+;; (anything 'anything-c-source-bm-global-use-candidates-in-buffer)
+(defvaralias 'anything-c-source-bm-global 'anything-c-source-bm-global-use-candidates-in-buffer)
+;; (anything 'anything-c-source-bm-global)
+
+(defun anything-c-bm-global-init ()
+  "Init function for `anything-c-source-bm-global'."
+  (when (require 'bm nil t)
+    (with-no-warnings
+      (let ((files bm-repository)
+            (buf (anything-candidate-buffer 'global)))
+        (dolist (file files)            ;ﾌﾞｯｸﾏｰｸされてるﾌｧｲﾙﾘｽﾄから，1つ取り出す
+          (dolist (bookmark (cdr (assoc 'bookmarks (cdr file)))) ;1つのﾌｧｲﾙに対して複数のﾌﾞｯｸﾏｰｸがあるので
+            (let ((position (cdr (assoc 'position bookmark))) ;position
+                  (annotation (cdr (assoc 'annotation bookmark))) ;annotation
+                  (file (car file))                               ;file名を取り出す
+                  line
+                  str)
+              (setq str (with-current-buffer (find-file-noselect file) ;anything用の文字列にformat
+                               (goto-char position)
+                               (beginning-of-line)
+                               (unless annotation
+                                   (setq annotation ""))
+                               (if (string= "" line)
+                                   (setq line  "<EMPTY LINE>")
+                                 (setq line (car (split-string (thing-at-point 'line) "[\n\r]"))))
+                               (format "%s:%d: [%s]: %s\n" file (line-number-at-pos) annotation line)))
+              (with-current-buffer buf (insert str)))))))))
+
+
+; anythingの検索対象を設定 
+;(setq anything-sources
+;      '(anything-c-source-buffers+
+;	anything-c-source-file-name-history
+;	anything-c-source-buffer-not-found
+;	anything-c-source-imenu
+;	anything-c-source-etags-select
+;	anything-c-source-plocate
+;	anything-c-source-bm-global
 ;	anything-c-source-home-locate
 ;;	anytihng-c-source-gtags-select
 ;;	anything-c-source-yaetags-select
-	))
+;	))
 
 ;; HOME以下のファイルをanythingから探す設定
 (defvar anything-c-source-home-locate
@@ -46,3 +88,25 @@
     (type . file)
     (requires-pattern . 3)
     (delayed)))
+
+;; my-anything
+(defun my-anything ()
+  (interactive)
+  (anything-other-buffer '(
+;; 			   anything-c-source-buffers+
+;; 			   anything-c-source-file-name-history
+;; 			   anything-c-source-recentf
+;;  			   anything-c-source-buffer-not-found
+;; 			   anything-c-source-imenu
+; 			   anything-c-source-etags-select
+ 			   anything-c-source-bm-global
+;;  			   anything-c-source-plocate
+;;  			   anything-c-source-home-locate
+			   anything-c-source-ipa-global
+			   ;;	anytihng-c-source-gtags-select
+			   ;;	anything-c-source-yaetags-select			   
+			   )
+			 "*my anything*"))
+(define-key global-map (kbd "C-:") 'my-anything)
+
+
--- dot_emacs/system/font-settings.el
+++ dot_emacs/system/font-settings.el
@@ -1,15 +1,71 @@
 ;;;; フォントに関する設定 ======================================================
-;;; フォント設定(Xを使用 and Emacs 23 の場合のみ設定)
-(cond ((and window-system (string-match "GNU Emacs 23\\." (emacs-version)))
-;;;        (set-default-font "Osaka－等幅-9")
-;;;        (set-fontset-font (frame-parameter nil 'font)
-;;; 			 'japanese-jisx0208
-;;; 			 '("Osaka－等幅-9" . "unicode-bmp"))))
-       (set-default-font "Bitstream Vera Sans Mono-6")
-       (set-fontset-font (frame-parameter nil 'font)
- 			 'japanese-jisx0208
- 			 '("VL ゴシック" . "unicode-bmp"))))
-;;;        (set-default-font "MeiryoKe-Gothic-6")
-;;;        (set-fontset-font (frame-parameter nil 'font)
-;;; 			 'japanese-jisx0208
-;;; 			 '("MeiryoKe_Gothic" . "unicode-bmp"))))
+;; ;;; フォント設定(Xを使用 and Emacs 23 の場合のみ設定)
+;; (cond ((and window-system (string-match "GNU Emacs 23\\." (emacs-version)))
+;; ;;;        (set-default-font "Osaka－等幅-9")
+;; ;;;        (set-fontset-font (frame-parameter nil 'font)
+;; ;;; 			 'japanese-jisx0208
+;; ;;; 			 '("Osaka－等幅-9" . "unicode-bmp"))))
+;;        (set-default-font "Bitstream Vera Sans Mono-6")
+;;        (set-fontset-font (frame-parameter nil 'font)
+;;  			 'japanese-jisx0208
+;;  			 '("VL ゴシック" . "unicode-bmp"))))
+;; ;;;        (set-default-font "MeiryoKe-Gothic-6")
+;; ;;;        (set-fontset-font (frame-parameter nil 'font)
+;; ;;; 			 'japanese-jisx0208
+;; ;;; 			 '("MeiryoKe_Gothic" . "unicode-bmp"))))
+
+;;;; テスト用
+;; (when (>= emacs-major-version 23)
+;;  (set-face-attribute 'default nil
+;;                      :family "monaco"
+;;                      :height 140)
+;;  (set-fontset-font
+;;   (frame-parameter nil 'font)
+;;   'japanese-jisx0208
+;;   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+;;  (set-fontset-font
+;;   (frame-parameter nil 'font)
+;;   'japanese-jisx0212
+;;   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+;;  (set-fontset-font
+;;   (frame-parameter nil 'font)
+;;   'mule-unicode-0100-24ff
+;;   '("monaco" . "iso10646-1"))
+;;  (setq face-font-rescale-alist
+;;       '(("^-apple-hiragino.*" . 1.2)
+;;         (".*osaka-bold.*" . 1.2)
+;;         (".*osaka-medium.*" . 1.2)
+;;         (".*courier-bold-.*-mac-roman" . 1.0)
+;;         (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+;;         (".*monaco-bold-.*-mac-roman" . 0.9)
+;;         ("-cdac$" . 1.3))))
+
+;; テスト用2
+;; (create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal" nil "menlokakugo")
+;; (set-fontset-font "fontset-menlokakugo"
+;;                   'unicode
+;;                   (font-spec :family "Hiragino Kaku Gothic ProN" :size 16)
+;;                   nil
+;;                   'append)
+;; (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+
+;; テスト用3
+(set-face-attribute 'default nil
+		    :family "Monaco"
+		    :height 140)
+		    ;:height 90)
+(set-fontset-font "fontset-default"
+		  'japanese-jisx0208
+		  '("Osaka" . "iso10646-1"))
+(set-fontset-font "fontset-default"
+		  'katakana-jisx0201
+		  '("Osaka" . "iso10646-1"))
+(setq face-font-rescale-alist
+      '((".*Monaco-bold.*" . 1.0)
+	(".*Monaco-medium.*" . 1.0)
+	(".*Osaka-bold.*" . 1.2)
+	(".*Osaka-medium.*" . 1.2)
+	("-cdac$" . 1.4)))
+
+
+
